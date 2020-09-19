0c2a22b6d7621b38131997d6b004c127705add11
Rokan Uddin <rokanu55@gmail.com>
2020-09-20 02:31:31 +0600

    fetch data from api & used

--- src/App.js
+++ src/App.js
@@ -8,19 +8,49 @@ const API_key= "492fbe1fd058a258ad7871724856f7f6";
 class App extends React.Component{
   constructor() {
     super();
-    this.state= {}
+    this.state= {
+      city:undefined,
+      country:undefined,
+      description:undefined,
+      temp:undefined,
+      min_temp:undefined,
+      max_temp:undefined
+    }
     this.getWeather();
   }
 
+  getTemperature(api_temp) {
+    const cellcias_temp=Math.floor(api_temp-273);
+    return cellcias_temp;
+  }
+
   getWeather = async() => {
-    const api_call= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_key}`);
+    const api_call= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid=${API_key}`);
     const res=await api_call.json();
     console.log(res);
+    this.setState({
+      city:res.name,
+      country:res.sys.country,
+      description:res.weather[0].main,
+      temp:this.getTemperature(res.main.temp),
+      min_temp:this.getTemperature(res.main.temp_min),
+      max_temp:this.getTemperature(res.main.temp_max)
+    });
   }
   render() {
     return (
     <div className="App">
-      <Weather />
+      <Weather 
+      city={this.state.city} 
+      country={this.state.country} 
+      description={this.state.description}
+      temp={this.state.temp}
+      min_temp={this.state.min_temp}
+      max_temp={this.state.max_temp}
+       />
+      
+      
+    
     </div>
   ); 
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faCloudSunRain } from '@fortawesome/free-solid-svg-icons';
-const Weather =() => {
+const Weather =(props) => {
 	return(
 		<div className="container">
 		<div className="cards">
-		<h1> Bangladesh </h1>
+		<h1> {props.city}, {props.country} </h1>
 		<h5 className="py-4">  <FontAwesomeIcon icon={faCloudSunRain} className="fa-5x" /></h5>
-		<h1 className="py-2"> 25&deg; </h1>
+		<h1 className="py-2"> {props.temp}&deg; </h1>
 		<h3> 
-		<span className="px-4"> 15&deg; </span>
-		<span className="px-4"> 30&deg; </span>
+		<span className="px-4"> {props.min_temp}&deg; </span>
+		<span className="px-4"> {props.max_temp}&deg; </span>
 		 </h3>
-		 <h4 className="py-2">Slow rain </h4>
+		 <h4 className="py-2">{props.description} </h4>
 		</div>
 		</div>
 
