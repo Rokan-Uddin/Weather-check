6ba536e54b0b363d6827d2365c19bca5f65757e5
Rokan Uddin <rokanu55@gmail.com>
2020-09-20 01:39:09 +0600

    setup a api

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Weather from './components/Weather';
+const API_key= "492fbe1fd058a258ad7871724856f7f6";
+// api_call="api.openweathermap.org/data/2.5/weather?q=London,uk";
 
-function App() {
-  return (
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state= {}
+    this.getWeather();
+  }
+
+  getWeather = async() => {
+    const api_call= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_key}`);
+    const res=await api_call.json();
+    console.log(res);
+  }
+  render() {
+    return (
     <div className="App">
       <Weather />
     </div>
-  );
+  ); 
+  }
+
 }
 
 export default App;
