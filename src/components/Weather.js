070f7dbe15446a820503346791dcbb0617d467de
Rokan Uddin <rokanu55@gmail.com>
2020-09-19 10:36:16 +0600

    Static design complete
    Add fontawsome and bootstrap

---
+++ src/components/Weather.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+const Weather =() => {
+	return(
+		<div className="container">
+		<div className="cards">
+		<h1> Bangladesh </h1>
+		<h5 className="py-4"> <FontAwesomeIcon icon="coffee" /></h5>
+		<h1 className="py-2"> 25&deg; </h1>
+		<h3> 
+		<span className="px-4"> 15&deg; </span>
+		<span className="px-4"> 30&deg; </span>
+		 </h3>
+		 <h4 className="py-2">Slow rain </h4>
+		</div>
+		</div>
+
+		);
+}
+export default Weather;
