15d87422314eb4433275557188ab6059b379bd62
Rokan Uddin <rokanu55@gmail.com>
2020-09-20 01:02:42 +0600

    Change Fonticon5 with react component
    size depends on "className" property of Fontawesome component
    icon-image depends on "icon" property of Fontawesome component
    normal fontawesome: <i class="fas fa-cloud-sun-rain"> </i>
    React fontawesome: <Fontawesome icon="faCloudSunRain" />

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Weather from './components/Weather';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fab, faCheckSquare, faCoffee)
-
 
 function App() {
   return (
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faCloudSunRain } from '@fortawesome/free-solid-svg-icons';
 const Weather =() => {
 	return(
 		<div className="container">
 		<div className="cards">
 		<h1> Bangladesh </h1>
-		<h5 className="py-4"> <FontAwesomeIcon icon="coffee" /></h5>
+		<h5 className="py-4">  <FontAwesomeIcon icon={faCloudSunRain} className="fa-5x" /></h5>
 		<h1 className="py-2"> 25&deg; </h1>
 		<h3> 
 		<span className="px-4"> 15&deg; </span>
