8ed18097844389456dc9a8c7c2113b7fbbc0cb14
Rokan Uddin <rokanu55@gmail.com>
2020-09-21 23:50:29 +0600

    Add Forms.js,set background

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import './weather.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faCloudSunRain } from '@fortawesome/free-solid-svg-icons';
+import {  faCloudSunRain,faCloudSun,faCloudRain,faCloudShowersHeavy,faCloud,faSun } from '@fortawesome/free-solid-svg-icons';
 const Weather =(props) => {
+	if(props.cityname!=null) { 
 	return(
-		<div className="container">
-		<div className="cards">
-		<h1> {props.city}, {props.country} </h1>
-		<h5 className="py-4">  <FontAwesomeIcon icon={faCloudSunRain} className="fa-5x" /></h5>
-		<h1 className="py-2"> {props.temp}&deg; </h1>
+		<div className="container text-light mt-5">
+		<div className="Card" >
+		<div className="CardBody">
+		<h1> {props.cityname}, {props.country} </h1>
+		<h5 className="py-4">  <FontAwesomeIcon icon={faCloudRain} className="fa-5x" /></h5>
+		<h1 className="py-2"> {props.temp_celsius}&deg; </h1>
 		<h3> 
-		<span className="px-4"> {props.min_temp}&deg; </span>
-		<span className="px-4"> {props.max_temp}&deg; </span>
+		<span className="px-4"> {props.temp_min}&deg; </span>
+		<span className="px-4"> {props.temp_max}&deg; </span>
 		 </h3>
 		 <h4 className="py-2">{props.description} </h4>
 		</div>
 		</div>
+		</div>
 
-		);
+		); }
+	else {
+		return("");
+	}
 }
 export default Weather;
